09a0e4f854378d83e2559c596b403dbf2ab3f648
hokuyama <h.okuyama800827@gmail.com>
2016-01-31 21:08:10 +0900

    push test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,9 +54,9 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user.destroy
+    @user.destroy!
     respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.html { redirect_to @user, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
